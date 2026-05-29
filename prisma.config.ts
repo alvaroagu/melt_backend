14f84115c16cba1e9ec91df15ad69868c6dbe7e0
avaroagu <alvaro.aguinagal@gmail.com>
2026-05-28 23:07:11 -0400

    fix: errors in prisma config and ts config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from '@prisma/config'
+import 'dotenv/config'
+import { defineConfig, env } from '@prisma/config'
 
 export default defineConfig({
   schema: './prisma/schema.prisma',
-  datasource: { url: process.env.DATABASE_URL },
+  datasource: { url: env('DATABASE_URL') },
   migrations: { path: 'prisma/migrations', seed: 'node prisma/seed.js' },
 })
